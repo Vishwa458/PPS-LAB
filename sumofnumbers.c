f31c10e0f77b9e66b5ce2d267afc3eb18d5377a1
Vishwa458 <svishwa805@gmail.com>
2024-05-14 11:31:50 +0530

    adding sumofnumbers.c

---
+++ sumofnumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num,i,sum=0;
+    printf("enter the number: ");
+    scanf("%d",&num);
+    for(i=0;i<=num;i++)
+    {
+       sum=sum+i;
+    }
+    printf("sum of all the numbers upto a given number is = %d",sum);
+}
