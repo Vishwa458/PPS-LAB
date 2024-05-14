d288864fa6a99fb1de2f40e14b1e42d1aad677ea
Vishwa458 <svishwa805@gmail.com>
2024-05-14 11:00:45 +0530

    adding 2 programs

---
+++ revofno.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num,rev,rem;
+    printf("Enter a number to find the reverse of it");
+    scanf("%d",&num);
+    while(num>0)
+    {
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+    }
+    printf("reverse of a number is = %d ",rev);
+    return 0;
+}
---
+++ sumofdig.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num,sum,rem;
+    printf("enter a number to find the sum of its digits: ");
+    scanf("%d",&num);
+    while(num>0)
+    {
+        rem=num%10;
+        sum=sum+rem;
+        num=num/10;
+    }
+    printf("Sum of digits is %d ",sum);
+    return 0;
+}
