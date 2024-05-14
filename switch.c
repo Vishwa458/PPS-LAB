122c6f46e7c50f2ce292a1c4bef8f376b19a0bf5
Vishwa458 <svishwa805@gmail.com>
2024-05-14 13:53:57 +0530

    added switch.c

---
+++ switch.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int no1,res,no2;
+    char op;
+    printf("enter operator: + - * / ");
+    scanf("%c",&op);
+    printf("enter 2 numbers: ");
+    scanf("%d %d",&no1,&no2);
+
+    switch(op)
+    {
+        case '+': res=no1+no2;
+                printf("sum is: %d",res);
+                break;
+        case '-': printf("sum is: %d",no1-no2);
+                break;
+        case '*': printf("sum is: %d",no1*no2);
+                break;
+        case '/': printf("sum is: %d",no1/no2);
+                break;
+        case '%': printf("sum is: %d",no1%no2);
+                break;
+        //case '+': printf("sum is: %d",no1+no2);
+               // break;
+               default:printf("please enter the correct operation");
+               break;
+
+    }
+
+}
