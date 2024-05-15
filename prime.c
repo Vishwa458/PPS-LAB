8de40a48ea25ca1a6b4c254bd6d7f3321f896a9a
Vishwa458 <svishwa805@gmail.com>
2024-05-15 15:00:07 +0530

    added prime program

---
+++ prime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,flag=1;
+    printf("enter the number to check: ");
+    scanf("%d",&n);
+    for(i=2;i<n/2;i++)
+    {
+        if(n%i==0)
+        flag=0;
+    }
+    if(flag==1)
+    printf("number is prime: ");
+    else
+    printf("number is not prime: ");
+    return 0;
+}
