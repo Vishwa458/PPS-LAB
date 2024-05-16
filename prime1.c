08485a4cb93c64e45e71f13f1da209d43c9de437
Vishwa458 <svishwa805@gmail.com>
2024-05-16 15:10:52 +0530

    added prime different way

--- prime1.c
+++ prime1.c
@@ -4,7 +4,7 @@ int main()
     int n,i,count=0;
     printf("enter the number to check: ");
     scanf("%d",&n);
-    for(i=2;i<n/2;i++)
+    for(i=2;i<=n/2;i++)
     {
         if(n%i==0)
         count++;
