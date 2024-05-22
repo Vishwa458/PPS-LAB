3ef23142e7f38d7f78754455a46afa0f7fa608fc
Vishwa458 <svishwa805@gmail.com>
2024-05-22 10:36:07 +0530

    Added program to find Area and circumference of circle

---
+++ Areaandcircumferenceofcircle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float pi=3.14,r=0.0,area=0.0,circumference=0.0;
+    printf("Enter radius: ");
+    scanf("%f",&r);
+    area=pi*r*r;
+    circumference=2*pi*r;
+
+    printf("Area = %.2f",area);
+    printf("Circumferenec = %.2f",circumference);
+
+    return 0;
+}
